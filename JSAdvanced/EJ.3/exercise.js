3ce47cd5136a5499d5db3745ee2e3c8003a78f27
LuisTM98 <luistortosam@gmail.com>
2023-10-18 12:32:27 +0200

    terminado ejercicio 3

--- JSAdvanced/EJ.3/exercise.js
+++ JSAdvanced/EJ.3/exercise.js
@@ -14,6 +14,15 @@ const divide = function (a, b) {
   return a / b;
 }
 
-const log = function (value) {
-  console.log(value);
-}
+// const log = function (value) {
+//   console.log(value);
+// }
+
+const sumArrow = (a,b) => a + b;
+const subtArrow = (a,b) => a - b;
+const multArrow = (a,b) => a * b;
+const divArrow = (a,b) => a / b;
+
+let value = divArrow(subtArrow(multArrow(sumArrow(2,4),sumArrow(5,2)),2),5)
+
+console.log(value)

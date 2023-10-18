6544d796ffad6b56d7646e017a4254970263e8ce
LuisTM98 <luistortosam@gmail.com>
2023-10-18 12:01:57 +0200

    terminado ejercicio 1 js

--- JSAdvanced/EJ.1/exercise.js
+++ JSAdvanced/EJ.1/exercise.js
@@ -4,7 +4,29 @@
 // developer => 1500
 // default => 1000
 function calculateSalary(role) {
-  // ...
+  let salary = 0;
+  switch (role) {
+    case 'ceo':
+      salary = '2200€';
+      break;
+
+    case 'manager':
+      salary = '1800€';
+    break;
+
+    case 'cto':
+      salary = '1800€';
+    break;
+
+    case 'developer':
+      salary = '1500€';
+    break;
+    
+    default:
+      salary = 'That is not a given role, but it probably earns 1000€'
+      break;
+  };
+  return salary
 }
 
 const ceoSalary = calculateSalary('ceo');

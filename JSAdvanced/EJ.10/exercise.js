f634cf535a06e313f5cbf2d4f85d444c928a72bb
LuisTM98 <luistortosam@gmail.com>
2023-10-19 11:04:25 +0200

    terminado ejercicio 10

--- JSAdvanced/EJ.10/exercise.js
+++ JSAdvanced/EJ.10/exercise.js
@@ -1,5 +1,9 @@
 const order = {};
 
-if (order && order.customer && order.customer.address && !order.customer.address.city) {
-  console.log('City is required');
+// if (order && order.customer && order.customer.address && !order.customer.address.city) {
+//   console.log('City is required');
+// }
+
+if(order?.customer?.address?.city === undefined){
+  console.log('City is required. I know you did not fulfilled it even before beginning to check the "order" element.')
 }

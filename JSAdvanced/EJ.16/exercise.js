3be4aba9a261ff1f3ff9a66322da04515f2cadb3
LuisTM98 <luistortosam@gmail.com>
2023-10-27 12:06:00 +0200

    terminado ejercicio 16 javascript

--- JSAdvanced/EJ.16/exercise.js
+++ JSAdvanced/EJ.16/exercise.js
@@ -1,4 +1,9 @@
-// Class definition
+class Person {
+    constructor(firstName, lastname){
+        this.firstName = firstName
+        this.lastName = lastname
+    }
+}
 
 const developer = new Person('Mario', 'Rossi');
 console.log(developer.firstName + " " + developer.lastName);

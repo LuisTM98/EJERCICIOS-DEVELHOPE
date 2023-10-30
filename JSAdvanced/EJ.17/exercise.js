56de09cf3132d6bdc700b848a043e8c73353a63c
LuisTM98 <luistortosam@gmail.com>
2023-10-30 11:11:44 +0100

    terminado ej. 17

--- JSAdvanced/EJ.17/exercise.js
+++ JSAdvanced/EJ.17/exercise.js
@@ -5,7 +5,14 @@ class Person {
   }
 }
 
+class Developer extends Person {
+  constructor(firstName, lastName, role){
+    super(firstName, lastName)
+    this.role = role
+  } 
+}
+
 const developer = new Developer("Mario", "Rossi", "Front-end");
 console.log(
-  developer.firstName + " " + developer.lastName + " " + developer.role
+  developer.firstName + " " + developer.lastName + ", " + developer.role
 );

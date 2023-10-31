b3b23df2730e537992ecec834d0ffe9b1cbe7ebf
LuisTM98 <luistortosam@gmail.com>
2023-10-31 10:15:31 +0100

    terminado ejercicio 19

--- JSAdvanced/EJ.19/exercise.js
+++ JSAdvanced/EJ.19/exercise.js
@@ -1,8 +1,24 @@
 class Person {
-  // ...
+  firstName = ""
+  lastName = ""
+  age = 0
+  constructor(firstName, lastName, age){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+  }
+  set age(newVal){
+    if (newVal < 1) {this.age = 1}
+    else if (newVal > 110) {this.age = 110}
+    else {this.age = newVal}
+  }
+
+  get fullName() {
+    return this.firstName + " " + this.lastName
+  }
 }
 
-const person = new Person('Mario', 'Rossi', 25);
+const person = new Person('Mario', 'Rossi', -255);
 console.log(person.fullName);
 
 person.firstName = 'Maria';

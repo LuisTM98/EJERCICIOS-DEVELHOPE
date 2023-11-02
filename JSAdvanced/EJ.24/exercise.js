abe93d22a08a1f3872e228cf3f87b765559223ac
LuisTM98 <luistortosam@gmail.com>
2023-11-02 14:18:59 +0100

    terminado ejercicio 24'

--- JSAdvanced/EJ.24/exercise.js
+++ JSAdvanced/EJ.24/exercise.js
@@ -1,9 +1,10 @@
 const person = {
   id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
+  name: 'Mario',      //changed firstName to name
+  surname: 'Rossi',   //changed lastName to surname
+  old: 25             //changed age to old
 };
 
-const { id, name, surname, old } = person;
+const { id, name, surname, old } = person
+
 console.log(id, name, surname, old);

b207cf123fe27b0b00d84f7b2fc3a4822fa22030
LuisTM98 <luistortosam@gmail.com>
2023-11-02 12:39:05 +0100

    trasteado ejercicio 28, aun no ha salido, preguntar dudad

--- JSAdvanced/EJ.28/exercise.js
+++ JSAdvanced/EJ.28/exercise.js
@@ -2,5 +2,12 @@ function sum(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
-const numbers = [1, 2, 3];
+const numbers = [1, 2, 3, 4, 5];
+
 console.log(sum(numbers[0], numbers[1], numbers[2]));
+
+function sumAgain(givenArray){
+  return givenArray.reduce((a,b) => a + b)
+}
+
+console.log(sumAgain(numbers))

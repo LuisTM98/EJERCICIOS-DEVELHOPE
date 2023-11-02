0caaa34b6b66ae20c27c37a26b17964e2403bfa2
LuisTM98 <luistortosam@gmail.com>
2023-11-02 13:52:10 +0100

    terminado ej. 21 '

--- JSAdvanced/EJ.21/exercise.js
+++ JSAdvanced/EJ.21/exercise.js
@@ -6,5 +6,11 @@ console.log('Before swap: ', num1, num2); // Before swap: 10 20
 let temp = num2;
 num2 = num1;
 num1 = temp;
-
 console.log('After swap: ', num1, num2); // After swap: 20 10
+
+//Swapping back, but with the destructuring
+
+let a, b;
+[b, a] = [num1, num2];
+[num1, num2] = [a, b];
+console.log('after swapping back: ', num1, num2)

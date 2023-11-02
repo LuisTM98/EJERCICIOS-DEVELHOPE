53845e8ec9f54bac27e39023f1ae3dbb51fa505f
LuisTM98 <luistortosam@gmail.com>
2023-11-02 17:57:52 +0100

    terminado ejercicio 27

--- JSAdvanced/EJ.27/exercise.js
+++ JSAdvanced/EJ.27/exercise.js
@@ -5,11 +5,14 @@ const person = {
   age: 25
 };
 
-const id = person.id;
-const personInfo = {
-  firstName: person.firstName,
-  lastName: person.lastName,
-  age: person.age
-};
+// const id = person.id;
+// const personInfo = {
+//   firstName: person.firstName,
+//   lastName: person.lastName,
+//   age: person.age
+// };
+
+const {id, ...rest} = person //creamos un objeto de dos variables, asignando a la segunda todo aquello que no es id
+const personInfo = rest // asignamos el valor de la segunda variable a la variable que llama el console.log de mas abajo
 
 console.log(id, personInfo);

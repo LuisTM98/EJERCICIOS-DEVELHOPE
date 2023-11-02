9949cc801013b83019aa01ec82e59a8bbf447db1
LuisTM98 <luistortosam@gmail.com>
2023-11-02 12:40:51 +0100

    añadido contenido ej. 28

--- JSAdvanced/EJ.28/exercise.js
+++ JSAdvanced/EJ.28/exercise.js
@@ -11,3 +11,11 @@ function sumAgain(givenArray){
 }
 
 console.log(sumAgain(numbers))
+
+// function sum(...nums) {
+//   const suma = nums.reduce((a,b) => a + b);
+//   return suma;
+// }
+
+// const numbers = [1, 2, 3];
+// console.log(sum(...numbers));

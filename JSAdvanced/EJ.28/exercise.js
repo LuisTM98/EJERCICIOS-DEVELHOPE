2380f9cf051a4bcb5d9cc7cf1cdadee175225cb1
LuisTM98 <luistortosam@gmail.com>
2023-11-02 18:09:13 +0100

    resuelto ej. 28

--- JSAdvanced/EJ.28/exercise.js
+++ JSAdvanced/EJ.28/exercise.js
@@ -1,17 +1,19 @@
-function sum(num1, num2, num3) {
-  return num1 + num2 + num3;
+function sum(...nums) {
+  return nums.reduce((a,b) => a+b);
 }
 
-const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3];
 
 console.log(sum(numbers[0], numbers[1], numbers[2]));
 
-function sumAgain(givenArray){
-  return givenArray.reduce((a,b) => a + b)
-}
+//La resolucion vista en clase no termina de ser correcto. Mas 
+//abajo esta comentado el codigo de clase. El ejercicio pide que
+//refactoricemos el codigo con el spread de tal manera que el 
+//console.log no varie su resultado en la consola, pero
+//sin variar el contenido del console.log como tal.
 
-console.log(sumAgain(numbers))
 
+// SOLUCION PROPUESTA EN CLASE:
 // function sum(...nums) {
 //   const suma = nums.reduce((a,b) => a + b);
 //   return suma;

b55775ca811ea000cf7a27244cf5552ebcfb8bed
LuisTM98 <luistortosam@gmail.com>
2023-11-02 18:45:55 +0100

    terminado ej. 29...?

--- JSAdvanced/EJ.28/exercise.js
+++ JSAdvanced/EJ.28/exercise.js
@@ -2,7 +2,7 @@ function sum(...nums) {
   return nums.reduce((a,b) => a+b);
 }
 
-const numbers = [1, 2, 3];
+const numbers = [1, 2, 3, 4, 6];
 
 console.log(sum(numbers[0], numbers[1], numbers[2]));
 
@@ -11,6 +11,8 @@ console.log(sum(numbers[0], numbers[1], numbers[2]));
 //refactoricemos el codigo con el spread de tal manera que el 
 //console.log no varie su resultado en la consola, pero
 //sin variar el contenido del console.log como tal.
+//El ejercicio no pide variar el resultado, sino unicamente
+//refactorizar la funcion sum con el spread operator
 
 
 // SOLUCION PROPUESTA EN CLASE:

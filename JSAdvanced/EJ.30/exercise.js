4a12860715e063aa3d3989574b0fb349858bcd4c
LuisTM98 <luistortosam@gmail.com>
2023-11-03 11:01:17 +0100

    terminado ej. 30 de la forma incorrecta, preguntar

--- JSAdvanced/EJ.30/exercise.js
+++ JSAdvanced/EJ.30/exercise.js
@@ -9,8 +9,20 @@ class Person {
   toJson() {
     return JSON.stringify(this);
   }
+
+  // fromJson(parameter){
+  //   let {id, firstName, lastName, age} = JSON.parse(parameter)
+  //   let temp = {id, firstName, lastName, age}
+  //   return new Person(temp)
+  // }
 }
 
+
 const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
-const developer = Person.fromJson(json);
-console.log(developer);
+// const developer = Person.fromJson(json);   
+
+// console.log(developer);
+
+let {id, firstName, lastName, age} = JSON.parse(json)
+let developer = new Person(id, firstName, lastName, age)
+console.log(developer)

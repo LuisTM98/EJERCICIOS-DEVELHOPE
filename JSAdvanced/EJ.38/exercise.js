ac67d1ce0ce39760248621533f8734ee6d87a421
LuisTM98 <luistortosam@gmail.com>
2023-11-15 10:18:26 +0100

    terminado ej. 38

--- JSAdvanced/EJ.38/exercise.js
+++ JSAdvanced/EJ.38/exercise.js
@@ -1,1 +1,21 @@
 const isLogged = true;
+
+function firstPromise(loggedVar){
+    return new Promise((resolved, rejected) => {
+        if (loggedVar === true) {resolved(Math.random())}
+        else {rejected(new Error("Is not logged"))}
+    })
+}
+
+function secondPromise(isLoggedResult){
+    return new Promise((resolved, rejected) => {
+        if (isLoggedResult > 0.5) {resolved({name: 'John', age: 24})}
+        else {rejected(new Error("Number is below 0.5"))}
+    })
+}
+
+firstPromise(isLogged)
+    .then(secondPromise)
+    .then((val) => console.log(val))
+    .catch((err) => console.log(err))
+

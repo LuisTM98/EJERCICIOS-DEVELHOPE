54d11ccea98c7eaa133e7ccec0b805f70acdb624
LuisTM98 <luistortosam@gmail.com>
2023-11-15 09:35:44 +0100

    terminado ej. 37 javascript

--- JSAdvanced/EJ.37/exercise.js
+++ JSAdvanced/EJ.37/exercise.js
@@ -1,1 +1,10 @@
 const number = 15;
+
+let thePromise = new Promise((resolve, reject) => {
+    if(number > 10) {resolve(number)}
+    else {reject(number)}
+})
+
+thePromise
+    .then((val) => console.log(val))
+    .catch(() => console.error('Error: number is below 10'))

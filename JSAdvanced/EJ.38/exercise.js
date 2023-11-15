5edfaa56282b3f2428c8c2354d5df65ccbaeee68
LuisTM98 <luistortosam@gmail.com>
2023-11-15 10:29:30 +0100

    terminado ej. 39

--- JSAdvanced/EJ.38/exercise.js
+++ JSAdvanced/EJ.38/exercise.js
@@ -2,15 +2,19 @@ const isLogged = true;
 
 function firstPromise(loggedVar){
     return new Promise((resolved, rejected) => {
-        if (loggedVar === true) {resolved(Math.random())}
-        else {rejected(new Error("Is not logged"))}
+        setTimeout(() => {
+            if (loggedVar === true) {resolved(Math.random())}
+            else {rejected(new Error("Is not logged"))}
+        }, 1000)
     })
 }
 
 function secondPromise(isLoggedResult){
     return new Promise((resolved, rejected) => {
-        if (isLoggedResult > 0.5) {resolved({name: 'John', age: 24})}
-        else {rejected(new Error("Number is below 0.5"))}
+        setTimeout(() => {
+            if (isLoggedResult > 0.5) {resolved({name: 'John', age: 24})}
+            else {rejected(new Error("Number is below 0.5"))}
+        }, 1000) 
     })
 }
 
--- JSAdvanced/EJ.39/exercise.js
+++ JSAdvanced/EJ.39/exercise.js
@@ -1,1 +1,25 @@
+const isLogged = true;
 
+function firstPromise(loggedVar){
+    return new Promise((resolved, rejected) => {
+        setTimeout(() => {
+            if (loggedVar === true) {resolved(Math.random())}
+            else {rejected(new Error("Is not logged"))}
+        }, 1000)
+    })
+}
+
+function secondPromise(isLoggedResult){
+    return new Promise((resolved, rejected) => {
+        setTimeout(() => {
+            if (isLoggedResult > 0.5) {resolved({name: 'John', age: 24})}
+            else {rejected(new Error("Number is below 0.5"))}
+        }, 1000) 
+    })
+}
+
+firstPromise(isLogged)
+    .then(secondPromise)
+    .then((val) => console.log(val))
+    .catch((err) => console.log(err))
+    .finally(() => console.log("hope everything worked out!")) //this is the only line I added since I already used the Error class

b7b292655ac2ad1337dec5b5be58fbccd2057ba4
LuisTM98 <luistortosam@gmail.com>
2023-12-17 12:13:27 +0100

    redundancia resuelta. ejercicio eventos terminado.

--- React/src/AlertClock.jsx
+++ React/src/AlertClock.jsx
@@ -1,9 +1,17 @@
 export function AlertClock(){
-    function handleShowTime(){
+    function showTime(){
         const now = new Date()
         alert(`The time right now is ${now}`)
     }
     return (
-        <button onClick={handleShowTime}>Click me!</button>
+        <Button label={'Click me!'} buttonWasClicked={showTime}></Button>
     )
+}
+
+// El componente boton usado arriba, es uno definido abajo. AlertClock asigna la funcion showTime a la propiedad buttonWasClicked.
+// Dicha propiedad viene establecida en el boton, y esta asignada al evento OnClick.
+// El componente App6 llama unicamente a AlertClock, y la propiedad onclick viene asociada directamente al boton
+
+export function Button({buttonWasClicked, label}){
+    return <button onClick={buttonWasClicked}>{label}</button>
 }

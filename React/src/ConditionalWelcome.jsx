072a9fd28d2ed285e7efbd9331147eda5dde65f8
LuisTM98 <luistortosam@gmail.com>
2023-12-17 10:29:00 +0100

    terminado ejercicio conditional 2

--- React/src/ConditionalWelcome.jsx
+++ React/src/ConditionalWelcome.jsx
@@ -5,9 +5,12 @@ export function WelcomeCond({name, age}){
         <div>
             <p>Welcome, <strong>{name}</strong></p>
             {(age>=18) && <AgeFunc years={age} />}
-            {age && <AgeFunc years={age}/>}
-            {(age>=18) && (age<65) && <AgeFunc years={age}/>}
-            {(age>=18) && (age<65) && name === 'John' && <AgeFunc years={age}/>}
+            {/* {age && <AgeFunc years={age}/>} */}
+            {/* {(age>=18) && (age<65) && <AgeFunc years={age}/>} */}
+            {/* {(age>=18) && (age<65) && name === 'John' && <AgeFunc years={age}/>} */}
+            {(age<18) && <p>You are very young!</p>} 
         </div>
     )
 }
+
+//Now it has the conditional with less than 18 years

79f8b15fe19581a12493e13d909b1861dbc5c5bc
LuisTM98 <luistortosam@gmail.com>
2023-12-17 10:09:35 +0100

    minor changes

--- React/src/Age.jsx
+++ React/src/Age.jsx
@@ -1,3 +1,5 @@
 export function AgeFunc({years}){
     return <p>Your age is {years}, probably</p>
 }
+
+//The years variable is referred to in the App4 component as age, but here can be named whatever one wants.

114fdd746f79f9a73b4de8c20489fafd7b7a52f2
LuisTM98 <luistortosam@gmail.com>
2024-01-12 09:39:08 +0100

    terminado ejercicio Refs 3

--- React/src/Counter.jsx
+++ React/src/Counter.jsx
@@ -1,23 +1,29 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { CounterDisplay } from "./CounterDisplay"
 
 export function Counter({leProp, initialValue}){
+    var leRefCounter = useRef(0)
+
     const [ counter, setter ] = useState(initialValue)
 
     useEffect(() => {
         console.log(`The counter value is ${counter}`)
+        console.log(leRefCounter)
     }, [counter])
     
     function handleIncrement(){
         setter(counter + 1)  //this has to be a function because the variable cant be updated each time otherwise in React
+        leRefCounter.current = 'sum up!'
     }
 
     function handleDecrement(){
         setter(counter - leProp)
+        leRefCounter.current = 'downing!'
     }
 
     function handleReset(){
         setter(initialValue)
+        leRefCounter.current = 'resetting'
     }
 
     return (

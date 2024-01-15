f489ad0ea0ca0c3ae799035c36c5025db92d0c97
LuisTM98 <luistortosam@gmail.com>
2024-01-15 09:53:54 +0100

    terminado ejercicio list and state 3

--- React/src/TodoList.jsx
+++ React/src/TodoList.jsx
@@ -1,26 +1,36 @@
 import { useState } from "react";
 
 export function TodoList() {
-  const [inputContent, setInputContent] = useState("");
+  const [inputContent, setInputContent] = useState('');
 
   function handleInputChange(event) {
     setInputContent(event.target.value);
   }
 
-  const [todosArray, setTodosArray] = useState([
+  const baseTodosArray = [
     "Wash the food",
     "Clean the dishes",
     "Sleep",
     "Un-microwave the car",
     "Kill John Connor",
     "Sleep but latter",
-  ]);
+  ]
+
+  const [todosArray, setTodosArray] = useState(baseTodosArray);
 
   function handleFormSubmit(event) {
     event.preventDefault();
-    const newTodosArray = [...todosArray, inputContent]
-    console.log(newTodosArray)
-    setTodosArray(newTodosArray);
+    const showedTodosArray = [...todosArray, inputContent]
+    console.log(showedTodosArray)  //for debug
+    setTodosArray(showedTodosArray);
+    setInputContent('')
+  }
+
+  function handleFormReset(event){
+    event.preventDefault()
+    const resetTodosArray = [...baseTodosArray]
+    setTodosArray(resetTodosArray)
+    setInputContent('')
   }
 
   return (
@@ -31,9 +41,10 @@ export function TodoList() {
         ))}
       </ul>
 
-      <form onSubmit={handleFormSubmit}>
-        <input type="text" name="addinput" onChange={handleInputChange} />
+      <form onSubmit={handleFormSubmit} onReset={handleFormReset}>
+        <input type="text" name="addinput" onChange={handleInputChange} value={inputContent}/>
         <button type="submit">Add to Todo list</button>
+        <button type="reset">Reset</button>
       </form>
     </div>
   );
